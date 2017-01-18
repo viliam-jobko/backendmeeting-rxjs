c743ec4648ef4be3a6b9918994872ed4abcb9938
Viliam Jobko <viliam.jobko@gmail.com>
2017-01-18 20:00:56 +0100

    Task 3 solution (Observables)

--- index-observables.js
+++ index-observables.js
@@ -7,7 +7,8 @@ const intervalStream = Rx.Observable.interval(5000).startWith(1);
 
 const magicNumbersStream = intervalStream
   .flatMap(() => Rx.Observable.fromPromise(superagent('http://127.0.0.1:3005/api')))
-  .map(data => data.body.magicNumber);
+  .map(data => data.body.magicNumber)
+  .distinctUntilChanged();
 
 const subscription = magicNumbersStream.subscribe(
   (magicNumber) => console.log(`${moment().format('HH:mm:ss')}    Magic number is: ${magicNumber}`),
