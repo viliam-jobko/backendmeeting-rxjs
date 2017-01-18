326be769b3b90c5179a444395ffc1c0e91e434cb
Viliam Jobko <viliam.jobko@gmail.com>
2017-01-18 19:57:00 +0100

    Task 1 solution (Observables)

---
+++ index-observables.js
@@ -0,0 +1,16 @@
+const Rx = require('rx');
+const superagent = require('superagent');
+const moment = require('moment');
+
+
+const intervalStream = Rx.Observable.interval(5000);
+
+const magicNumbersStream = intervalStream
+  .flatMap(() => Rx.Observable.fromPromise(superagent('http://127.0.0.1:3005/api')))
+  .map(data => data.body.magicNumber);
+
+const subscription = magicNumbersStream.subscribe(
+  (magicNumber) => console.log(`${moment().format('HH:mm:ss')}    Magic number is: ${magicNumber}`),
+  (err) => console.log(`Error: ${err}`),
+  () => console.log('Completed.')
+);
