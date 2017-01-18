005804b426ae22a092bc91614ec07b82e18479c2
Viliam Jobko <viliam.jobko@gmail.com>
2017-01-18 19:56:47 +0100

    Task 1 solution (Promises)

---
+++ index-promises.js
@@ -0,0 +1,18 @@
+const superagent = require('superagent');
+const moment = require('moment');
+
+
+const doJob = async () => {
+  const data = await getDataFromApi();
+  const magicNumber = data.magicNumber;
+
+  console.log(`${moment().format('HH:mm:ss')}    Magic number is: ${magicNumber}`);
+};
+
+const getDataFromApi = async () => {
+  const data = await superagent('http://127.0.0.1:3005/api');
+  return data.body;
+};
+
+
+setInterval(doJob, 5000);
