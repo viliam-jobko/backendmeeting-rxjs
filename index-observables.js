6a01d8b911a5bc014501eaff29f893a3bc660748
Viliam Jobko <viliam.jobko@gmail.com>
2017-01-18 20:03:51 +0100

    Task 4 solution (Observables)

--- index-observables.js
+++ index-observables.js
@@ -8,10 +8,11 @@ const intervalStream = Rx.Observable.interval(5000).startWith(1);
 const magicNumbersStream = intervalStream
   .flatMap(() => Rx.Observable.fromPromise(superagent('http://127.0.0.1:3005/api')))
   .map(data => data.body.magicNumber)
-  .distinctUntilChanged();
+  .distinctUntilChanged()
+  .takeWhile(magicNumber => magicNumber < 101);
 
 const subscription = magicNumbersStream.subscribe(
   (magicNumber) => console.log(`${moment().format('HH:mm:ss')}    Magic number is: ${magicNumber}`),
   (err) => console.log(`Error: ${err}`),
-  () => console.log('Completed.')
+  (magicNumber) => console.log(`${moment().format('HH:mm:ss')}    Magic number is too high! I better quit checking it. It's: ${magicNumber}`)
 );
